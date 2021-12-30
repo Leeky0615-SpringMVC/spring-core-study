d845f5a4f5d077c9eb1d89e95ea1ddc105c98891
Leeky0615 <rbdus7174@naver.com>
2021-12-30 17:18:33 +0900

    feat : test&apply log trace with proxy pattern V2-use concrete class

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v1_config.InterfaceProxyConfig;
+import hello.proxy.config.v2_config.ConcreteProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,8 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 //@Import({AppV1Config.class,AppV2Config.class})
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
