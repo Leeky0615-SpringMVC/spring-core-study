921d56ad74c72491ec641b5b0da351d75d47ac36
Leeky0615 <rbdus7174@naver.com>
2021-12-31 16:27:06 +0900

    feat : study how to apply jdk dynamic proxy with filter

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,7 @@ package hello.proxy;
 import hello.proxy.config.v1_config.InterfaceProxyConfig;
 import hello.proxy.config.v2_config.ConcreteProxyConfig;
 import hello.proxy.config.v2_dynamicProxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicProxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-//@Import(InterfaceProxyConfig.class)
 //@Import({AppV1Config.class,AppV2Config.class})
-//@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
+//@Import(ConcreteProxyConfig.class) // 추상클래스 프록시 패턴 적용
+//@Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
+@Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
@@ -27,3 +29,5 @@ public class ProxyApplication {
 	}
 
 }
+
+
