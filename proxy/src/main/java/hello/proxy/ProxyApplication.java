d2e8e2bfa9cbe607d425845672dce8b2850b4ecb
Leeky0615 <rbdus7174@naver.com>
2022-01-01 19:06:03 +0900

    feat : study how to apply proxyFactory (V1::interface, V2::concrete)

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,18 +4,23 @@ import hello.proxy.config.v1_config.InterfaceProxyConfig;
 import hello.proxy.config.v2_config.ConcreteProxyConfig;
 import hello.proxy.config.v2_dynamicProxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicProxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.InitBinder;
 
 //@Import({AppV1Config.class,AppV2Config.class})
 //@Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
-//@Import(ConcreteProxyConfig.class) // 추상클래스 프록시 패턴 적용
+//@Import(ConcreteProxyConfig.class) // 구체클래스 프록시 패턴 적용
 //@Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
-@Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
+//@Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
+//@Import(ProxyFactoryConfigV1.class) // proxyFactory -> 인터페이스(jdkDynamicProxy)
+@Import(ProxyFactoryConfigV2.class) // proxyFactory -> 구체클래스(CGLIB)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
