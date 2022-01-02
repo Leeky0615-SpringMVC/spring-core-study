f38800eee7f12e7ec84dc84ee19fed3b80c429c6
Leeky0615 <rbdus7174@naver.com>
2022-01-02 15:49:19 +0900

    feat : study how to apply bean post processor to V1(interface),V2(concrete),v3(component scan)

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,13 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-// @Import({AppV1Config.class,AppV2Config.class})
-// @Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
-// @Import(ConcreteProxyConfig.class) // 구체클래스 프록시 패턴 적용
-// @Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
-// @Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
-// @Import(ProxyFactoryConfigV1.class) // proxyFactory -> 인터페이스(jdkDynamicProxy)
-@Import(ProxyFactoryConfigV2.class) // proxyFactory -> 구체클래스(CGLIB)
+//@Import({AppV1Config.class,AppV2Config.class})
+//@Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
+//@Import(ConcreteProxyConfig.class) // 구체클래스 프록시 패턴 적용
+//@Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
+//@Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
+//@Import(ProxyFactoryConfigV1.class) // proxyFactory -> 인터페이스(jdkDynamicProxy)
+//@Import(ProxyFactoryConfigV2.class) // proxyFactory -> 구체클래스(CGLIB)
+@Import(BeanPostProcessorConfig.class) // beanPostProcessor
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
