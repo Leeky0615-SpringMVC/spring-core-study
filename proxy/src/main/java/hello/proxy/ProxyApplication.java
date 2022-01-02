04a14c527a893deb5b460a24274b99f358f514fb
Leeky0615 <rbdus7174@naver.com>
2022-01-02 14:48:45 +0900

    chore : add space

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -14,12 +14,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.InitBinder;
 
-//@Import({AppV1Config.class,AppV2Config.class})
-//@Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
-//@Import(ConcreteProxyConfig.class) // 구체클래스 프록시 패턴 적용
-//@Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
-//@Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
-//@Import(ProxyFactoryConfigV1.class) // proxyFactory -> 인터페이스(jdkDynamicProxy)
+// @Import({AppV1Config.class,AppV2Config.class})
+// @Import(InterfaceProxyConfig.class) // 인터페이스 클래스 프록시 패턴 적용
+// @Import(ConcreteProxyConfig.class) // 구체클래스 프록시 패턴 적용
+// @Import(DynamicProxyBasicConfig.class) // 기본 동적 프록시 적용 (reflection)
+// @Import(DynamicProxyFilterConfig.class) // 동적 프록시 필터 적용 (reflection)
+// @Import(ProxyFactoryConfigV1.class) // proxyFactory -> 인터페이스(jdkDynamicProxy)
 @Import(ProxyFactoryConfigV2.class) // proxyFactory -> 구체클래스(CGLIB)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
